68bdd051f54b187e65e005d8c95827585ccd2f1c
lavendermp <lavendermp@lavendermps-iMac.local>
2021-08-27 16:51:14 +0700

    jumbotron, with local styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Jumbotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron';
 
 export default function App() {
   return (
-    <Jumbotron />
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.Title>{item.subTitle}</Jumbotron.Title>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
-
-
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import {Container, Item, Inner, Pane, Title, subTitle, Image} from './styles/jumbotron';
 
-export default function Jumbotron({}) {
-  return (<p>I am the Jumbotron</p>)
+// Compount componnets need children.
+export default function Jumbotron({children, direction='row',...restProps}) {
+  return (
+    <Item direction={direction}>
+      <Inner>
+        {children}
+      </Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}){
+  return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}){
+  return <Pane {...restProps}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}){
+  return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.subTitle = function JumbotronsubTitle({children, ...restProps}){
+  return <subTitle {...restProps}>{children}</subTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({children, ...restProps}){
+  return <Image {...restProps}/>
 }
