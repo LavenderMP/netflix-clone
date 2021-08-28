f227512fd093a5b6abd4713a19c0a36e010295ba
lavendermp <lavendermp@lavendermps-iMac.local>
2021-08-29 06:53:41 +0700

    accordion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {JumbotronContainer} from './containers/jumbotron';
+import { FaqsContainer } from './containers/faqs';
+import { FooterContainers } from './containers/footer';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return <JumbotronContainer />;
+  return <>
+  <JumbotronContainer />
+  <FaqsContainer />
+  <FooterContainers />
+  </>;
 }
